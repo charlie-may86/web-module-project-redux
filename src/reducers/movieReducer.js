cb6f274510afb8ac00b0853bb708144ec8628bd8
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2021-07-23 08:08:18 -0500

    add movie, including a movie.id is working

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,20 +1,45 @@
-import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
-import movies from './../data.js';
+import { ADD_MOVIE, DELETE_MOVIE } from "../actions/movieActions.js";
+import movies from "./../data.js";
 
 const initialState = {
-    movies: movies,
-    appTitle: "IMDB Movie Database"
-}
+  movies: movies,
+  appTitle: "IMDB Movie Database",
+};
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case DELETE_MOVIE:
-            return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case DELETE_MOVIE:
+      return {
+        movies: state.movies.filter((item) => action.payload !== item.id),
+      };
+    case ADD_MOVIE:
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+         action.payload
+        ],
+      };
+    // case ADD_MOVIE:
+    //     return {
+    //         ...state,
+    //         movies: [
+    //             ...state.movies,
+    //             {action.payload}
+    //         ],
+
+    //     }
+    default:
+      return state;
+  }
+};
 
 export default reducer;
+
+
+// id: movies.length + 1,
+// title: action.payload.title,
+// director: action.paylod.director,
+// metascore: action.payload.metascore,
+// genre: action.payload.genre,
+// description: action.payload.description
